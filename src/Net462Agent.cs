7e9b1a246d6a2c5cace52eeb76bd4c2268ece93e
Charlie Poole <charliepoole@gmail.com>
2023-09-11 22:18:02 -0700

    Use testcentric signing key rather than nunit

--- src/Net462Agent.cs
+++ src/Net462Agent.cs
@@ -3,17 +3,10 @@
 // Licensed under the MIT License. See LICENSE file in root directory.
 // ***********************************************************************
 
-using System;
-using System.Diagnostics;
-using System.IO;
-using System.Security;
-using TestCentric.Engine.Agents;
-using TestCentric.Engine.Internal;
-using TestCentric.Engine.Communication.Transports.Remoting;
-
 namespace TestCentric.Agents
 {
     public class Net462Agent : TestCentricAgent<Net462Agent>
-    {        public static void Main(string[] args) => TestCentricAgent<Net462Agent>.Execute(args);
+    {
+        public static void Main(string[] args) => TestCentricAgent<Net462Agent>.Execute(args);
     }
 }
